dd475596f0c50090b50f363d53a69036923c7002
BLalves59 <167263927+BLalves59@users.noreply.github.com>
2024-08-06 22:47:30 -0300

    ok
    ok

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,10 +3,31 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className='container'>
+    <div className='containerGlobal'>
 
       <div className={styles.containerTitulo}>
         <h1 className={styles.txtTitulo}>Aula next</h1>  
+        <h2>Exemplo2</h2>
+        <h3>Exemplo3</h3>
+        <p>Exemplo paragrafo</p>
+
+        {/* <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>  OUUU..... */}
+          <div className={styles.containerImagem}>
+           <Image
+            src={"/arvore.jpg"}
+            width={100}
+            height={100}
+            alt="Imagem da arvore"
+            className={styles.imageHome}
+          />
+          <Image
+            src={"/arvore.jpg"}
+            width={1000}
+            height={1000}
+            alt="Imagem da arvore"
+            className={styles.imageHome}
+          />
+        </div>
       </div>
       
     </div>
